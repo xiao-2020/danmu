a61a3cf1c0797a4b75b128c19bf3352f5e039c53
xiao-2020 <liuxiao_7451321@163.com>
2018-10-22 20:26:04 +0800

    this

--- src/left.js
+++ src/left.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 class Left extends React.Component{
+  constructor(props) {
+    super(props)
+    console.log(this.props)
+  }
   render() {
     return <p>lef213123213t</p>
   }
 }
 export default Left
-
