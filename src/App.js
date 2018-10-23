b75239f0cf09f78bf579b40de84d90eca973756d
xiao-2020 <415559133@qq.com>
2018-10-23 13:19:40 +0800

    Obj.component test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css';
 import Test from './test'
 import Left from './left'
+import ObjectComponet from './objectComponemt'
 class App extends Component {
   constructor() {
     super();
@@ -19,6 +20,7 @@ class App extends Component {
         <button onClick={this.click}>点我弹窗口</button>
         <p>{this.state.num}</p>
         {this.state.showTips && <Test num= { this.state.num }  cancle= { this.cancel } setNum= {this.setNum} left={ <Left /> }>1209</Test>}
+        <ObjectComponet.a a={1} />
       </div>
     );
   }
---
+++ src/objectComponemt.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const component = {
+  a: function (props) {
+    return <p>this is a component {props.a}</p>
+  }
+}
+
+export default component
