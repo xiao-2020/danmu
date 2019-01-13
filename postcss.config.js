c3db8f83e5750afc8199615b43c69a2a0620fc01
xiao-2020 <liuxiao_7451321@163.com>
2019-01-13 17:28:42 +0800

    postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,13 @@
+
 // 导出postcss 默认的配置对象
 module.exports = {
   // 使用哪些插件
-  plugins: {
-    // 自动添加前缀的插件  后面使用  {}  代表使用默认配置
-     "autoprefixer": {}
-  }
+  // 自动添加前缀的插件  后面使用  {}  代表使用默认配置
+  //  "autoprefixer": {},  具体插件要控制的浏览器规则 可以在根目录下 创建一个 .browserlistrc文件 也可以在package。json 里面 加一个  browerslisst 指定
+  // plugins: {
+  //   "autoprefixer": {},
+  // },
+  plugins: [
+    require('autoprefixer')
+  ]
 }
